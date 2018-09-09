65bf233856f0e70ed1e4f05ee78ac92216f6f321
Anoop P V <anooppv567@gmail.com>
2018-09-09 21:52:46 +0530

    added services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {MatDialog} from '@angular/material'
+import {MatDialog} from '@angular/material';
+import { UserService } from '../services/user.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,15 +10,26 @@ import {MatDialog} from '@angular/material'
 export class LoginComponent implements OnInit {
   username: string;
   password: string;
-  constructor(private router: Router) { }
+  constructor(private router: Router , private userService : UserService) { }
 
   ngOnInit() {
   }
   login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(['home']);
-    }else {
-      alert('Invalid credentials');
-    }
+    // if(this.username == 'admin' && this.password == 'admin'){
+    //  this.router.navigate(['home']);
+    // }else {
+    //   alert('Invalid credentials');
+    // }
+    let userDetails:any = {};
+    userDetails.username = this.username;
+    userDetails.password = this.password;
+    this.userService.validateUserDetails(userDetails).subscribe(response =>{
+      if(response){
+        this.router.navigate(['home']);
+      }
+      else {
+          alert('Invalid credentials');
+        }
+    });
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { Observable ,of} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+ private userName = 'admin';
+ private password = 'admin';
   constructor() { }
+   validateUserDetails = (userDetails:any) : Observable<boolean> =>{
+    if(this.userName === userDetails.username && this.password === userDetails.password){
+      return of(true);
+    }
+    else{
+      return of(false);
+    }
+   }
 }
